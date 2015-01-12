bcf2837e42565d953bbdc840cadab2f4fa7a0216
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-01-12 14:04:55 -0700

    Added functionality to nearly all lib functions

--- lib.js
+++ lib.js
@@ -6,19 +6,34 @@ lib.generateHeading1 = function(text) {
 }
 
 lib.generateHeading = function(level, text) {
-    return "not yet implemented"
+    return "<h" + level + ">" + text + "</h" + level + ">"
 }
 
 lib.generateOrderedList = function(arrayOfText) {
-    return "not yet implemented"
+    var ol = "<ol>"
+    arrayOfText.forEach(function(item){
+        ol += "<li>" + item + "</li>"
+    })
+    ol += "</ol>"
+    return ol
 }
 
 lib.generateUnorderedList = function(arrayOfText) {
-    return "not yet implemented"
+    var ul = "<ul>"
+    arrayOfText.forEach(function(item){
+        ul += "<li>" + item + "</li>"
+    })
+    ul += "</ol>"
+    return ul
 }
 
 lib.generateTableRow = function (arrayOfText) {
-    return "not yet implemented"
+	var row = "<tr>"
+	arrayOfText.forEach(function(item){
+		row += "<td>" + item + "</td>"
+	})	
+	row += "</tr>"
+    return row
 }
 
 lib.generateTable = function(twoDimensionalArrayOfText) {
@@ -26,27 +41,35 @@ lib.generateTable = function(twoDimensionalArrayOfText) {
 }
 
 lib.generateHyperLink = function(url, text) {
-    return "not yet implemented"
+    return "<a href=\"" + url + "\">" + text + "</a>"
 }
 
 lib.generateFormTextField = function(name) {
-    return "not yet implemented"
-}
-
-lib.generateFormTextField = function(name) {
-    return "not yet implemented"
+    return "<input type=\"text\" name=\"" + name + "\">"
 }
 
 lib.generateFormTextFieldWithLabel = function(name, label) {
-    return "not yet implemented"
+    return "<label>" + label + "</label> <input type=\"text\" name=\"" + name + "\">"
 }
 
 lib.generateDropdownList = function(arrayOfValues, arrayOfText) {
-    return "not yet implemented"
+	var container = "<select>"
+	var idx = 0
+	arrayOfText.forEach(function(name){
+		value = arrayOfValues[idx]
+		container += "<option value=\"" + value + "\">" + name + "</option>"
+		idx++
+	})
+	container += "</select>"
+    return container
 }
 
 lib.generateYoutubeVideoEmbeddableFrame = function(width, height, videoId, allowfullscreen) {
-    return "not yet implemented"
+	var iframe = "<iframe "
+	var width = "width=\"" + width + "\" "
+	var height = "height=\"" + height + "\" "
+	var vidId = "src=\"//www.youtube.com/embed/" + videoId + "\" allowfullscreen></iframe>" 
+    return iframe + width + height + vidId
 }
 
 module.exports = lib
