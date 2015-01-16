8ea9a48fb6cb209e5518c1064ed2f0100841ba40
rshamirza <rshamirza@Rshas-MacBook-Pro.local>
2015-01-15 23:45:11 -0700

    Completed All Functions

--- lib.js
+++ lib.js
@@ -10,19 +10,39 @@ lib.generateHeading = function(level, text) {
 }
 
 lib.generateOrderedList = function(arrayOfText) {
-    return "not yet implemented"
+    var finalString = "<ol>";
+    for (var i = 0; i < arrayOfText.length; i++) {
+        finalString += "<li>" + arrayOfText[i] + "</li>";
+    }
+    finalString += "</ol>";
+    return finalString;
 }
 
 lib.generateUnorderedList = function(arrayOfText) {
-    return "not yet implemented"
+    var finalString = "<ul>" + "";
+    for (var i = 0; i < arrayOfText.length; i++) {
+        finalString += "<li>" + arrayOfText[i] + "</li>";
+    }
+    finalString += "</ul>";
+    return finalString;
 }
 
 lib.generateTableRow = function (arrayOfText) {
-    return "not yet implemented"
+    var finalString = "<tr>";
+    for (var i = 0; i < arrayOfText.length; i++) {
+        finalString += "<td>" + arrayOfText[i] + "</td>";
+    }
+    finalString += "</tr>";
+    return finalString; 
 }
 
 lib.generateTable = function(twoDimensionalArrayOfText) {
-    return "not yet implemented"
+    var finalString = "<table>";
+    for (var i = 0; i < twoDimensionalArrayOfText.length; i++) {
+        finalString += "<tr><td>" + twoDimensionalArrayOfText[i][0] + "</td><td>" + twoDimensionalArrayOfText[i][1] + "</td></tr>";
+    }
+    finalString += "</table>";
+    return finalString; 
 }
 
 lib.generateHyperLink = function(url, text) {
@@ -30,23 +50,28 @@ lib.generateHyperLink = function(url, text) {
 }
 
 lib.generateFormTextField = function(name) {
-    return "not yet implemented"
+    return '<form><input type="text" name="'+name+'"></form>';
 }
 
 lib.generateFormTextField = function(name) {
-    return "not yet implemented"
+    return '<form><input type="text" name="'+name+'"></form>';
 }
 
 lib.generateFormTextFieldWithLabel = function(name, label) {
-    return "not yet implemented"
+    return '<form>'+ label+':<br><input type="text" name="'+name+'"></form>';
 }
 
 lib.generateDropdownList = function(arrayOfValues, arrayOfText) {
-    return "not yet implemented"
+    var finalString = "<select>";
+    for (var i = 0; i < arrayOfValues.length; i++) {
+        finalString += '<option value="' + arrayOfValues[i] + '">'+arrayOfText[i]+'</option>';
+    }
+    finalString += "</select>";
+    return finalString; 
 }
 
 lib.generateYoutubeVideoEmbeddableFrame = function(width, height, videoId, allowfullscreen) {
-    return "not yet implemented"
+    return '<iframe width="'+width+'" height="'+height+'" src="http://www.youtube.com/embed/'+videoId+'" allowfullscreen="'+allowfullscreen+'"></iframe>'
 }
 
 module.exports = lib
